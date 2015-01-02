e911cc3ee3bd545e4836af65eb76223a642bcfaa
JobaerChowdhury <jobaer.du@gmail.com>
2015-01-02 15:40:41 +0600

    Solve list related exercises. Remaining is 'init'

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -47,19 +47,78 @@ object List { // `List` companion object. Contains functions for creating and wo
     foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
 
 
-  def tail[A](l: List[A]): List[A] = sys.error("todo")
+  def tail[A](l: List[A]): List[A] = l match {
+      case Nil => sys.error("empty list")
+      case Cons(x, xs) => xs
+  }
 
-  def setHead[A](l: List[A], h: A): List[A] = sys.error("todo")
+  def setHead[A](l: List[A], h: A): List[A] = l match {
+    case Nil => sys.error("empty list")
+    case Cons(x, xs) => Cons(h, xs)
+  }
 
-  def drop[A](l: List[A], n: Int): List[A] = sys.error("todo")
+  @annotation.tailrec
+  def drop[A](l: List[A], n: Int): List[A] =
+    if (n < 0) sys.error("Negative index")
+    else l match {
+      case Nil => sys.error("Empty list")
+      case Cons(x, xs) =>
+        if (n == 0) l
+        else drop(xs, n-1)
+    }
 
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = sys.error("todo")
+  @annotation.tailrec
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
+    case Cons(x, xs) if f(x) => dropWhile(xs, f)
+    case _ => l
+  }
+
+  def reverse[A](l: List[A]): List[A] = {
+    def go(l: List[A], acc: List[A]): List[A] = l match {
+      case Nil => acc
+      case Cons(x, xs) => go(xs, Cons(x, acc))
+    }
+
+    go(l, Nil)
+  }
 
   def init[A](l: List[A]): List[A] = sys.error("todo")
 
-  def length[A](l: List[A]): Int = sys.error("todo")
+  def length[A](l: List[A]): Int = {
+    @annotation.tailrec
+    def go(l: List[A], acc: Int): Int = l match {
+      case Nil => acc
+      case Cons(x, xs) => go(xs, acc+1)
+    }
 
-  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = sys.error("todo")
+    go(l, 0)
+  }
 
-  def map[A,B](l: List[A])(f: A => B): List[B] = sys.error("todo")
+  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = l match {
+    case Nil => z
+    case Cons(x, xs) => foldLeft(xs,f(z,x))(f)
+  }
+
+  def map[A,B](l: List[A])(f: A => B): List[B] = l match {
+    case Nil => Nil
+    case Cons(x, xs) => Cons(f(x), map(xs)(f))
+  }
+}
+
+object ListTest {
+  def main(args: Array[String]) = {
+    import List._
+    println("Testing list datastructure")
+
+    val testList = Cons(1, Cons(2, Cons(4, Nil)))
+
+    println(tail(testList))
+//    println(tail(Nil))
+    println(setHead(testList, 5))
+    println(drop(testList, 1))
+
+    println(dropWhile(testList, (x:Int) => x < 3))
+
+    println(reverse(testList))
+  }
 }
